e77121b2391abd4c4926fc4560ec07626aab627e
Sureshyarava <69299412+Sureshyarava@users.noreply.github.com>
2021-12-31 20:02:24 +0530

    Update Receiver.ino

--- Receiver.ino
+++ Receiver.ino
@@ -1,26 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+# include <RH_ASK.h>
+# include <SPI.h> 
+RH_ASK rf_driver;
+String vol;
+void setup()
+{
+    pinMode(9, OUTPUT);
+    rf_driver.init();
+   Serial.begin(9600);
+
 }
 
-void loop() {
-  float  ans=0;
-  int j=0;
-  for(int i=0;i<2000;i++){
-    int k=analogRead(2);
-    if (k>3065){
-    ans+=k;
-    j++;
-    }
-  }
-  ans=ans/j;
-  ans=ans-3112;
-  Serial.println("ans = "+String(ans));
-  if (ans>0){
-  Serial.println("Voltage = "+String(250*ans/403));
-  }
-  else{
-    Serial.println("Voltage = "+String(0));
-  }
-  delay(100);
-  }
+void loop()
+{
+    uint8_t buf[6];
+    uint8_t buflen = sizeof(buf);
+    if (rf_driver.recv(buf, & buflen))
+{
+    vol = String((char *)buf);
+float k = vol.toFloat();
+digitalWrite(9, HIGH);
+Serial.print("Vol:");
+Serial.println(k);
+Serial.print("Delay:");
+
+
+Serial.println((k * 1000) / 27.7);
+
+
+delay(((k * 1000) / 27.7) * 1000);
+
+
+digitalWrite(9, LOW);
+}
+
+}
